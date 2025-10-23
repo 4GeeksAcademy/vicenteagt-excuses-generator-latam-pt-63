d4400e3c2667ec955234c4dbc7cd3c154c3257af
VicenteAGT <155429830+VicenteAGT@users.noreply.github.com>
2025-10-23 00:26:05 +0000

    [Refactored Code by vicenteagt] -Changed functions name -Changed variables names -Eliminated declarations string variables and added direct

--- src/app.js
+++ src/app.js
@@ -5,37 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = generarNumero;
+window.onload = calculateRandomIndex;
 
-// arrays
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+let whos = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let actions = ['ate', 'peed', 'crushed', 'broke'];
+let whats = ['my homework', 'my phone', 'the car'];
+let whens = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-// funcion para calcular indice random
-function generarNumero(myArray) {
-    let posicion = Math.floor(Math.random() * myArray.length);
-    return posicion
+function calculateRandomIndex(excuseArray) {
+    let positionArray = Math.floor(Math.random() * excuseArray.length);
+    return positionArray
 }
 
-// generacion de indices dentro de cada array
+let randomWhos = calculateRandomIndex(whos);
+let randomActions = calculateRandomIndex(actions);
+let randomWhats = calculateRandomIndex(whats);
+let randomWhens = calculateRandomIndex(whens);
 
-let randomWho = generarNumero(who);
-let randomAction = generarNumero(action);
-let randomWhat = generarNumero(what);
-let randomWhen = generarNumero(when);
-
-// generar strings de cada array
-
-let stringWho = who[randomWho];
-let stringAction = action[randomAction];
-let stringWhat = what[randomWhat];
-let stringWhen = when[randomWhen];
-
-
-
-let resultado = (stringWho + " " + stringAction + " " + stringWhat + " " + stringWhen);
-document.getElementById("excuse").innerText = resultado
+let excuseGenerated = (whos[randomWhos] + " " + actions[randomActions] + " " + whats[randomWhats] + " " + whens[randomWhens]);
+document.getElementById("excuse").innerText = excuseGenerated
 
 
